49211563ccc2923d2a8397f696d5152d6fcf5e27
ascdqz <shengxin.jin@stonybrook.edu>
2022-11-14 10:31:25 -0500

    3d before due

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def preprocess1(data):
             possible_value[i].add(row[i])
     return possible_value #p=[{'feature0':'不重复出现数字‘}，{}...]
 
-def buildtree(con_id,dcheader,l1,l2,gi1,gi2,dad):
-    if len(l1)<=2 or len(l2)<=2:return
+def buildtree(con_id,dcheader,l1,l2,gi1,gi2,dad=Node(None, None, None, None)):
+    #if len(l1)<=2 or len(l2)<=2:return
     Name_son1 = (f'{dcheader[con_id[0]]} <={con_id[1]}')
     Name_son2 = (f'{dcheader[con_id[0]]} > {con_id[1]}')
     son1 = Node(Name_son1,len(l1),dad,gi1)
@@ -27,13 +27,16 @@ def buildtree(con_id,dcheader,l1,l2,gi1,gi2,dad):
     dad.children[Name_son2] = son2
     if gi1 < gi2:
         son2 = dad
-        a,b,c,d,e = splitdata(preprocess1(l2), l2)
-        buildtree(a,dcheader,b,c,d,e,dad)
-
+        z, x, c, v, b, best_gain2 = splitdata(preprocess1(l2), l2)
+        if best_gain2 >0:
+            buildtree(z,dcheader,x,c,v,b,dad)
+        else: return
     else:
         son1 = dad
-        a, b, c, d, e = splitdata(preprocess1(l1), l1)
-        buildtree(a, dcheader, b, c, d, e, dad)
+        z, x, c, v, b, best_gain1 = splitdata(preprocess1(l1), l1)
+        if best_gain1 >0:
+            buildtree(z,dcheader,x,c,v,b,dad)
+        else: return
 
 
 
@@ -43,35 +46,28 @@ def buildtree(con_id,dcheader,l1,l2,gi1,gi2,dad):
 
 
 def splitdata (possible_value,data):
+    global condition_id, ls1, ls2, gini1, gini2
     bestgini=shuji(data)
+    bestgain=0.0
     li1 = []
     li2 = []
-    exit_ = False
     for i, n in enumerate(possible_value):
-        o = 0
         for q in n:
-            o += 1
             for row in data:
                 if row[i]<=q:
                     li1.append(row)
                 else:li2.append(row)
-                gini1 = shuji(li1)
-                gini2 = shuji(li2)
-
-            xiao=min(gini1,gini2)
-            if gini1 == 0 or gini2 == 0:
-                condition_id = [i,q] #i=coloum number, q=specific value under i coloum
-                exit_ = True
-                break
-            if xiao<bestgini:
-                bestgini = gini1
+            gini1 = shuji(li1)
+            gini2 = shuji(li2)
+            gain = bestgini-(len(li1)/len(data))*gini1-(len(li2)/len(data))*gini2
+            if gain >bestgain:
+                bestgain = gain
                 condition_id = [i,q]
-                if i != len(possible_value)-1 and o != len(n):
-                    li1 = []
-                    li2 = []
-        if exit_:
-            break
-    return condition_id,li1,li2,gini1,gini2
+                ls1 = li1
+                ls2 = li2
+            li1 = []
+            li2 = []
+    return condition_id, ls1, ls2, gini1, gini2, bestgain
 
 def shuji(ww):
     gini=0
@@ -106,9 +102,10 @@ def load(path):
 
 
 if __name__ == '__main__':
-    Path = ('fishiris.csv')
-    Data,dchead = load(Path)
-    a, b, c, d, e = splitdata(preprocess1(Data), Data)
-    dad = Node(None, None, None, None)
-    buildtree(a, dchead, b, c, d, e, dad)
+    Path = 'fishiris.csv'
+    gdad=Node(None, None, None, None)
+    Data, dchead = load(Path)
+    a_, m_, op_, d_, e_, qwe_ = splitdata(preprocess1(Data), Data)
+    buildtree(a_, dchead, m_, op_, d_, e_, gdad)
+    # not stable use with caution
 ascd=1
